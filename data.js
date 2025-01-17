792a9c48b885f40d3160ee312ed875b8d8cb0b29
Stanton <stanwgov@gmail.com>
2025-01-17 16:11:59 +0200

    docs: add new file data.js
    This file stores the data points for the different charts in an array

---
+++ data.js
@@ -0,0 +1,170 @@
+var fig1=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [0.1,     50,   100000, 20000, null,  null, null],
+          [1,       50,   100000, 20000, 20000, null, null],
+          [1,       50,   100000, 20000, 2000,  5000, null],
+          [40,      50,   2500,   500,   50,    125,  null],
+          [100,     50,   1000,   200,   null,  50,   null],
+          [400,     50,   250,    50,    null,  null, null],
+          [2000,    50,   50,     null,  null,  null, null],
+          [10000,   50,   null,   null,  null,  null, null],
+          [20000,   50,   null,   null,  null,  null, null],
+          [30000,   50,   null,   null,  null,  null, null],
+          [1000000, 50,   null,   null,  null,  null, null],
+          [0,       null, null,   null,  null,  null, 0   ]
+        ];
+        
+var fig2=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [0.1,     50,   300000, 100000, null,   null,  null],
+          [1,       50,   300000, 100000, 100000, null,  null],
+          [1,       50,   300000, 100000, 5000,   20000, null],
+          [100,     50,   3000,   1000,   50,     200,   null],
+          [400,     50,   750,    250,    null,   50,    null],
+          [750,     50,   400,    133.33, null,   null,  null],
+          [2000,    50,   400,    50,     null,   null,  null],
+          [10000,   50,   400,    null,   null,   null,  null],
+          [20000,   50,   400,    null,   null,   null,  null],
+          [30000,   50,   400,    null,   null,   null,  null],
+          [1000000, 50,   400,    null,   null,   null,  null],
+          [0,       null, null,   null,   null,   null,  0   ]
+        ];
+        
+var fig3=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [0.1,     50,   null,    50000, null, null,  null],
+          [1,       50,   1000000, 50000, null, null,  null],
+          [1,       50,   20000,   50000, null, null,  null],
+          [20,      50,   1000,    50000, 1000, null,  null],
+          [400,     50,   50,      50000, 1000, null,  null],
+          [1000,    50,   null,    50000, 1000, null,  null],
+          [2000,    50,   null,    50000, 1000, null,  null],
+          [3000,    50,   null,    50000, 1000, null,  null],
+          [1000000, 50,   null,    50000, 1000, null,  null],
+          [0,       null, null,    null,  null, null,  0   ],
+          [null,       null, null,    null,  null, 0   ,  null   ],
+          [null,       null, null,    null,  null, null,  null   ]
+        ];
+       
+var fig4=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [0.1,     50,   100000, null,    null,  null,  null],
+          [10,      50,   100000, 100000,  null,  null,  null],
+          [10,      50,   100000, 1000000, null,  null,  null],
+          [20,      50,   50000,  null,    50000, null,  null],
+          [100,     50,   10000,  null,    50000, null,  null],
+          [1000,    50,   1000,   null,    50000, null,  null],
+          [2000,    50,   1000,   null,    50000, null,  null],
+          [3000,    50,   1000,   null,    50000, null,  null],
+          [10000,   50,   1000,   null,    50000, null,  null],
+          [100000,  50,   1000,   null,    50000, null,  null],
+          [1000000, 50,   1000,   null,    50000, null,  null],
+          [0,       null, null,   null,    null,  0   ,  0   ]
+        ];
+       
+var fig5=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [0.1,     50,   null,   null,  null,  null,  null],
+          [2,       50,   50,     null,  null,  null,  null],
+          [2,       50,   1000000, 2500,  3200,  null, null],
+          [6.25,    50,   null,   800,   3200,  3200,  null],
+          [100,     50,   null,   50,    3200,  200,   null],
+          [400,     50,   null,   null,  750,   50,    null],
+          [1000,    50,   null,   null,  300,   null,  null],
+          [1000,    50,   null,   null,  50,    null,  null],
+          [2000,    50,   null,   null,  null,  null,  null],
+          [3000,    50,   null,   null,  null,  null,  null],
+          [1000000, 50,   null,   null,  null,  null,  null],
+          [0,       null, null,   null,  null,  null,  0   ]
+        ];
+        
+var fig6=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [1,     50,   null,   null,  null,   null,  null],
+          [25,    50,   50,     null,  null,   null,  null],
+          [25,    50,   100000, 4000,  null,   null,  null],
+          [100,   50,   null,   1000,  100000, null,  null],
+          [100,   50,   null,   50,    3500,   null,  null],
+          [350,   50,   null,   null,  1000,   null,  null],
+          [350,   50,   null,   null,  50,     null,  null],
+          [1000,  50,   null,   null,  null,   null,  null],
+          [2000,  50,   null,   null,  null,   null,  null],
+          [3000,  50,   null,   null,  null,   null,  null],
+          [10000, 50,   null,   null,  null,   null,  null],
+          [0,     null, null,   null,  null,   0   ,  0   ]
+        ];
+        
+var fig7=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [1,     50,   null,    null,  null,    null,    null],
+          [32,    50,   1000000, null,  null,    null,    null],
+          [32,    50,   3125,    null,  null,    null,    null],
+          [100,   50,   1000,    null,  1000000, null,    null],
+          [100,   50,   1000,    null,    3500,    null,  null],
+          [250,   50,   400,     null,  1400,    1000000, null],
+          [250,   50,   400,     null,  1400,    2000,    null],
+          [2000,  50,   50,      null,  175,     250,     null],
+          [7000,  50,   null,    null,  50,      71.42857,null],
+          [10000, 50,   null,    null,  null,    50,      null],
+          [100000,50,   null,    null,  null,    null,    null],
+          [0,     null, null,    0,     null,    0,       0   ]
+        ];
+        
+var fig8=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [1,      50,   null,    null,  null, null,  null],
+          [25,     50,   1000000, null,  null, null,  null],
+          [25,     50,   8000,    50000, null, null,  null],
+          [200,    50,   1000,    50000, 1000, null,  null],
+          [4000,   50,   1000,    50000, 50,   null,  null],
+          [10000,  50,   1000,    50000, null, null,  null],
+          [20000,  50,   1000,    50000, null, null,  null],
+          [30000,  50,   1000,    50000, null, null,  null],
+          [40000,  50,   1000,    50000, null, null,  null],
+          [50000,  50,   1000,    50000, null, null,  null],
+          [100000, 50,   1000,    50000, null, null,  null],
+          [0,      null, null,    null,  null,    0,  0   ]
+        ];        
+        
+var fig9=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [1,      50,   null,    null,  null, null,  null],
+          [200,    50,   1000000, null,  null, null,  null],
+          [200,    50,   2500,    50000, null, null,  null],
+          [500,    50,   1000,    50000, null, null,  null],
+          [10000,  50,   1000,    50000, null, null,  null],
+          [20000,  50,   1000,    50000, null, null,  null],
+          [30000,  50,   1000,    50000, null, null,  null],
+          [40000,  50,   1000,    50000, null, null,  null],
+          [50000,  50,   1000,    50000, null, null,  null],
+          [60000,  50,   1000,    50000, null, null,  null],
+          [100000, 50,   1000,    50000, null, null,  null],
+          [0,      null, null,    null,  0,    0   ,  0   ]
+        ];
+        
+        
+var fig10=
+      [
+          ['Year', 'Sales', 'Expenses','f','g','h','i'],
+          [0.1,     50,   300000, 1500000,  null,  null,  null],
+          [600,     50,   50,     250,      null,  null,  null],
+          [3000,    50,   null,   50,       null,  null,  null],
+          [10000,   50,   null,   null,     null,  null,  null],
+          [100000,  50,   null,   null,     null,  null,  null],
+          [200000,  50,   null,   null,     null,  null,  null],
+          [300000,  50,   null,   null,     null,  null,  null],
+          [400000,  50,   null,   null,     null,  null,  null],
+          [500000,  50,   null,   null,     null,  null,  null],
+          [600000,  50,   null,   null,     null,  null,  null],
+          [1500000, 50,   null,   null,     null,  null,  null],
+          [0,       null, null,   null,     0   ,  0   ,  0   ]
+        ];
