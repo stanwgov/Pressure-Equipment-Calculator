4c8492be59ff595192735d187f2a45524bf9d547
Stanton <stanwgov@gmail.com>
2025-01-18 22:17:37 +0200

    bug: zoom of browser removed

--- graph.js
+++ graph.js
@@ -1,7 +1,7 @@
   google.charts.load('current', {'packages':['corechart']});
   google.charts.setOnLoadCallback(drawChart);
 
- 
+  
   function drawChart(fig) {
   
     //** Setup Default Chart*/ 
